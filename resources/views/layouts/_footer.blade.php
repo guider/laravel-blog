b493e608c09ce777575d883bdf6bee49a14d9172
张金伟 <guider@yeah.net>
2020-11-30 18:34:58 +0800

    split header and footer

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer"><img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt=""
+                            class="brand-icon"><a href="">stay flosh</a>
+    <div class="float-right"><a href="/about">关于</a></div>
+</footer>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,7 +3,7 @@
     <div class="jumbotron">
         <h1 >Hello Laravel</h1>
         <p class="lead">
-            你现在看到是的 <a href="www.baidu.com">百度</a>的演示
+            你现在看到是的 <a href="http://www.baidu.com">百度</a>的演示
         </p>
 
         <p>一切, 从这里开始</p>
