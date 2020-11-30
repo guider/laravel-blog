ad827b828b316bc22d81dc2ac9579e51a3b99b56
张金伟 <guider@yeah.net>
2020-11-30 17:22:03 +0800

    init style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>blog</title>
+    <title>@yield('title','myblog')</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+</nav>
     @yield('content')
 </body>
 </html>
