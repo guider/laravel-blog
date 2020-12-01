8b1cecf16af2c4063811af927db8ab19586207ab
张金伟 <guider@yeah.net>
2020-12-01 09:12:16 +0800

    user info

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -9,10 +10,16 @@ class UsersController extends Controller
     //
 
 
+    public function show(User $user) {
+
+        return view('users.show',compact('user'));
+    }
+
     public function create()
     {
 
         return view('users.create');
 
     }
+
 }
