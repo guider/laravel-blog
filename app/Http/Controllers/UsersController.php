a776099fe33a4dcf8e389958d060decfe171214a
张金伟 <guider@yeah.net>
2020-12-01 20:12:41 +0800

    add api auth

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,11 +9,17 @@ use Illuminate\Support\Facades\Auth;
 class UsersController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show', 'create', 'store']]);
+
+    }
 
 
-    public function show(User $user) {
+    public function show(User $user)
+    {
 
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
     }
 
     public function create()
@@ -24,36 +30,36 @@ class UsersController extends Controller
     public function store(Request $request)
     {
 
-        $this->validate($request,[
-           'name'=>'required|unique:users|min:3|max:50',
-           'email'=>'required|email|unique:users|max:255',
-           'password'=>'required|confirmed|min:6'
+        $this->validate($request, [
+            'name' => 'required|unique:users|min:3|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
         ]);
 
-        $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password),
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
         ]);
 
 
         Auth::login($user);
-        session()->flash('success','欢迎, 注册成功');
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '欢迎, 注册成功');
+        return redirect()->route('users.show', [$user]);
 
     }
 
     public function edit(User $user)
     {
-        return view('users.edit',compact('user'));
+        return view('users.edit', compact('user'));
     }
 
-    public function update(User $user,Request $request)
+    public function update(User $user, Request $request)
     {
 
-        $this->validate($request,[
-            'name'=>'required|max:50',
-            'password'=>'required|confirmed|min:6'
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
         ]);
 
 //        $user->update([
@@ -61,15 +67,15 @@ class UsersController extends Controller
 //            'password'=>$request->password
 //        ]);
 
-        $data=[];
-        $data['name']=$request->name;
-        if ($request->password){
-            $data['password']=bcrypt($request->password);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
         }
         $user->update($data);
 
-        session()->flash('success','个人资料更新成功!');
-        return redirect()->route('users.show',$user->id);
+        session()->flash('success', '个人资料更新成功!');
+        return redirect()->route('users.show', $user->id);
 
     }
 }
