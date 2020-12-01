07dc7c7ef9ef7c9c0b7e02e4b50f44be0cf1ad66
张金伟 <guider@yeah.net>
2020-12-01 20:27:34 +0800

    add policy

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,16 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+
+
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -22,12 +32,14 @@ class SessionsController extends Controller
         ]);
 
 
-        if (Auth::attempt($credentials,$request->has('remember'))) {
-            session()->flash('success','登录成功');
-            return redirect()->route('users.show',[Auth::user()]);
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '登录成功');
+            $fallback = route('users.show', [Auth::user()]);
+
+            return redirect()->intended($fallback);
         } else {
 
-            session()->flash('danger','用户名密码不匹配');
+            session()->flash('danger', '用户名密码不匹配');
             return redirect()->back()->withInput();
 
         }
@@ -37,7 +49,7 @@ class SessionsController extends Controller
     public function destory()
     {
         Auth::logout();
-        session()->flash('success','您已退出登录');
+        session()->flash('success', '您已退出登录');
         return redirect('login');
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,9 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['show', 'create', 'store']]);
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
 
     }
 
@@ -51,12 +54,14 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
 
+        $this->authorize('update',$user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
