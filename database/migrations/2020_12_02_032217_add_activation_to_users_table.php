3f13c60b291570877720faa2c5769361636a7306
张金伟 <guider@yeah.net>
2020-12-02 13:34:43 +0800

    fix mail

--- database/migrations/2020_12_02_032217_add_activation_to_users_table.php
+++ database/migrations/2020_12_02_032217_add_activation_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddActivationToUserTable extends Migration
+class AddActivationToUsersTable extends Migration
 {
     /**
      * Run the migrations.
