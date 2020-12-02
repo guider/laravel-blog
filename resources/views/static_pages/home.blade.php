f7faf1e704ad9e67673d8133264a26160873f4e6
张金伟 <guider@yeah.net>
2020-12-02 16:26:14 +0800

    weibo home

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,9 +5,16 @@
 
         <div class="row">
             <div class="col-md-8 section status_form">
-                @include('shared._status_form')
-            </div>
 
+
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+                o
+                <h4>微博列表</h4>
+                <hr>
+                @include('shared._feed')
+            </div>
 
             <aside class="col-md-4">
                 <section class="user_info">
